4594b5b35c56d6c2a754ebd1078b436e282f81bd
Perchinka <alexsator.lukin@gmail.com>
2024-10-03 16:26:18 +0100

    Add base for topics layout
    Add datatable with topics and basic sorting

--- design/layouts/topics.py
+++ design/layouts/topics.py
@@ -1,134 +1,92 @@
-import urwid
+from textual.app import App
+from textual.widgets import (
+    Label,
+    ListItem,
+    ListView,
+    Input,
+    DataTable,
+    Header,
+    Footer,
+)
+from textual.containers import Horizontal, Vertical
+from random import randint
 
-import random
-import uuid
-from typing import List
 
-from panwid.datatable import DataTable, DataTableColumn, DataTableDivider
+class TopicsView(App):
+    """Main Kafka interface TUI"""
 
+    BINDINGS = [
+        ("s", "sort_by_size", "Sort By Size"),
+        ("p", "sort_by_partitions", "Sort By Partitions"),
+    ]
 
-class RoundedBox(urwid.LineBox):
-    def __init__(self, body):
-        super().__init__(body, tlcorner="╭", trcorner="╮", blcorner="╰", brcorner="╯")
+    def compose(self):
+        yield Header()
 
-
-class HorizontalMenu(urwid.Padding):
-    def __init__(self):
-        menu = urwid.Columns(
-            [
-                urwid.SelectableIcon("Consumers"),
-                urwid.SelectableIcon("Topics"),
-                urwid.SelectableIcon("Brokers"),
-                urwid.SelectableIcon("Exit"),
-            ],
+        yield Horizontal(
+            Vertical(
+                ListView(
+                    ListItem(Label("Brokers")),
+                    ListItem(Label("Topics")),
+                    ListItem(Label("Consumers")),
+                ),
+                id="navigation-pane",
+            ),
+            Vertical(
+                Input(placeholder="Search Topics...", id="search-field"),
+                DataTable(
+                    id="topics-table",
+                ),
+                id="center-pane",
+            ),
         )
-        super().__init__(menu, left=2, right=100, min_width=100)
+        yield Footer()
 
+    current_sorts: set = set()
 
-class SearchBar(RoundedBox):
-    def __init__(self):
-        super().__init__(urwid.Edit("Search: "))
+    def sort_reverse(self, sort_type: str):
+        """Determine if `sort_type` is ascending or descending."""
+        reverse = sort_type in self.current_sorts
+        if reverse:
+            self.current_sorts.remove(sort_type)
+        else:
+            self.current_sorts.add(sort_type)
+        return reverse
 
+    async def on_mount(self):
+        """Called when app is mounted, populate the topics table with placeholders"""
+        topics_table = self.query_one("#topics-table", DataTable)
+        topics_table.cursor_type = "row"
+        topics_table.zebra_stripes = True
 
-class TopicsDataTable(RoundedBox):
-    def __init__(self, topics: List):
-        columns = [
-            DataTableColumn(
-                "topic",
-                "Topic",
-                width=("weight", 2),
-                align="center",
-                format_fn=lambda x: x.upper(),
-            ),
-            DataTableDivider("\N{BOX DRAWINGS DOUBLE VERTICAL}"),
-            DataTableColumn(
-                "partitions",
-                "Partitions",
-                align="center",
-                width=(13),
-            ),
-            DataTableDivider("\N{BOX DRAWINGS DOUBLE VERTICAL}"),
-            DataTableColumn(
-                "brokers",
-                "Brokers",
-                align="center",
-            ),
-            DataTableDivider("\N{BOX DRAWINGS DOUBLE VERTICAL}"),
-            DataTableColumn(
-                "size",
-                "Size",
-                align="center",
-            ),
-        ]
-        data = [
-            dict(topic=topic, partitions=partitions, brokers=brokers, size=size)
-            for topic, partitions, brokers, size in topics
-        ]
-        table = DataTable(
-            columns=columns,
-            data=data,
-            with_scrollbar=True,
-            sort_refocus=True,
-            cell_selection=True,
+        for col in (
+            "Topic name",
+            "Partitions",
+            "Replication Factor",
+            "Number of messages",
+            "Size",
+        ):
+            topics_table.add_column(col, key=col)
+
+        for i in range(10):
+            topics_table.add_row(
+                f"topic_{i}",
+                str(randint(1, 10)),
+                str(randint(1, 3)),
+                str(randint(0, 800)),
+                f"{i * 1024} KB",
+            )
+
+    def action_sort_by_size(self) -> None:
+        table = self.query_one(DataTable)
+        table.sort("Size", key=lambda x: x, reverse=self.sort_reverse("Size"))
+
+    def action_sort_by_partitions(self) -> None:
+        table = self.query_one(DataTable)
+        table.sort(
+            "Partitions", key=lambda x: x, reverse=self.sort_reverse("Partitions")
         )
-        super().__init__(urwid.Frame(table))
-
-
-class TopicsList(urwid.Pile):
-    def __init__(self, topics: List):
-        super().__init__([("pack", SearchBar()), TopicsDataTable(topics)])
-
-
-class TopicDetail(RoundedBox):
-    def __init__(self, topic: str):
-        body = urwid.Filler(urwid.Text(topic), valign="top")
-        super().__init__(body)
-
-
-class TopicsView(urwid.Columns):
-    def __init__(self, topics: List):
-        super().__init__([TopicsList(topics), TopicDetail("Topic detatils")])
-
-
-class Main:
-    def __init__(self, topics: List):
-        palette = [
-            ("table_row_body", "", ""),
-            ("table_row_body focused", "white", "black"),
-            ("table_row_body column_focused", "", "black"),
-            ("table_row_body highlight", "", ""),
-            ("table_row_body highlight focused", "", "black"),
-            ("table_row_body highlight column_focused", "", "black"),
-            ("table_row_header", "", ""),
-            ("table_row_header focused", "", ""),
-            ("table_row_header column_focused", "", "black"),
-            ("table_row_header highlight", "", "yellow"),
-            ("table_row_header highlight focused", "", "yellow"),
-            ("table_row_header highlight column_focused", "", "yellow"),
-            ("table_row_footer", "", "white"),
-            ("table_row_footer focused", "", "dark gray"),
-            ("table_row_footer column_focused", "", "black"),
-            ("table_row_footer highlight", "", "yellow"),
-            ("table_row_footer highlight focused", "", "yellow"),
-            ("table_row_footer highlight column_focused", "", "yellow"),
-        ]
-        layout = urwid.Frame(body=TopicsView(topics), header=HorizontalMenu())
-        self.loop = urwid.MainLoop(layout, palette=palette)
-
-    def run(self):
-        self.loop.run()
 
 
 if __name__ == "__main__":
-    # random data
-    n = 100
-    topics = [
-        (
-            uuid.uuid4().hex,
-            random.randint(1, 10),
-            random.randint(1, 10),
-            random.randint(1, 10),
-        )
-        for _ in range(n)
-    ]
-    Main(topics).run()
+    TopicsView().run()
