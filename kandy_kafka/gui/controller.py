e804e1dae907f2dc8016f5104adcb1deba95fffd
Perchinka <alexsator.lukin@gmail.com>
2024-10-07 15:47:19 +0100

    Add search field functionality

--- kandy_kafka/gui/controller.py
+++ kandy_kafka/gui/controller.py
@@ -13,7 +13,7 @@ from kandy_kafka.gui.views import TopicsView
 class Controller(App):
     """Main application controller"""
 
-    BINDINGS = [("f", "fetch", "fetch")]
+    BINDINGS = [("ctrl+r", "reload", "reload")]
     CSS_PATH = "app.tcss"  # Textual CSS file for styling
 
     def __init__(self, bootstraped: Bootstraped):
@@ -22,18 +22,16 @@ class Controller(App):
         self.kafka_adapter = self.bootstrapped.kafka_adapter
         self.view = TopicsView()  # Instantiate the TopicsView from views.py
 
+    async def on_mount(self):
+        await self.reload_topics()
+
     async def reload_topics(self):
-        """
-        Reload the topics by pulling them from the KafkaModel and updating the TopicsView.
-        """
         topics = self.kafka_adapter.get_topics()  # Fetch topics from Kafka
         self.view.show_topics(topics)  # Update the table in the view
 
-    async def action_fetch(self) -> None:
-        """Handle the message to reload topics."""
+    async def action_reload(self) -> None:
         await self.reload_topics()
 
     def compose(self) -> ComposeResult:
-        """Compose the app layout."""
         yield self.view
         yield Footer()
--- kandy_kafka/gui/views/topics.py
+++ kandy_kafka/gui/views/topics.py
@@ -1,4 +1,10 @@
+from kandy_kafka.domain.models import Topic
+
+import re
+
 from textual.containers import Horizontal, Vertical, Container
+from textual.reactive import reactive
+from textual import events
 from textual.app import ComposeResult
 from textual.widgets import (
     Label,
@@ -7,7 +13,6 @@ from textual.widgets import (
     Input,
     DataTable,
 )
-from kandy_kafka.domain.models import Topic
 
 
 class TopicsView(Container):
@@ -21,6 +26,8 @@ class TopicsView(Container):
     def __init__(self):
         super().__init__()
         self.table = DataTable(id="topics-table")
+        self.topics = []  # Store all topics
+        self.filtered_topics = reactive([])
 
     def build_table(self):
         """Build the DataTable for topics"""
@@ -39,15 +46,23 @@ class TopicsView(Container):
         ):
             topics_table.add_column(col, key=col)
 
-    def show_topics(self, topics: list[Topic]):
+    def update_table(self, topics: list[Topic]):
+        """Update the table with the provided list of topics."""
         self.table.clear()
-
         topics_table = self.query_one("#topics-table", DataTable)
 
         for topic in topics:
             topics_table.add_row(
                 topic.name, len(topic.partitions), 1, topic.amount_of_messages, 0
             )
+
+    def show_topics(self, topics: list[Topic]):
+        """Show the given list of topics in the table."""
+        self.topics = topics  # Store the topics for filtering
+        self.filtered_topics = (
+            topics  # Initially, filtered topics is the same as topics
+        )
+        self.update_table(self.filtered_topics)
 
     def compose(self) -> ComposeResult:
         """Compose the view with the table."""
@@ -79,7 +94,6 @@ class TopicsView(Container):
         return reverse
 
     async def on_mount(self):
-        """Called when app is mounted, populate the topics table with placeholders"""
         self.build_table()
 
     def action_sort_by_size(self) -> None:
@@ -95,3 +109,20 @@ class TopicsView(Container):
         table.sort(
             "Partitions", key=lambda x: int(x), reverse=self.sort_reverse("Partitions")
         )
+
+    async def on_input_changed(self, event):
+        """Called whenever the input in the search field changes."""
+        search_query = event.value.strip()
+
+        if search_query == "":
+            # If the search query is empty, show all topics
+            self.filtered_topics = self.topics
+        else:
+            # Use regex to filter topics based on search query
+            self.filtered_topics = [
+                topic
+                for topic in self.topics
+                if re.search(search_query, topic.name, re.IGNORECASE)
+            ]
+
+        self.update_table(self.filtered_topics)
