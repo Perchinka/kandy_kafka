6bea0a2cb29a349302addccbe2e71ed34edfd15f
Perchinka <alexsator.lukin@gmail.com>
2024-10-11 13:13:26 +0100

    Refactor controller and topics
    Add BaseTable class and two separate childs of this class: Topics and Messages tables, update CSS, remove redundant code from TopicsView

--- kandy_kafka/gui/controller.py
+++ kandy_kafka/gui/controller.py
@@ -5,7 +5,7 @@ from typing import TYPE_CHECKING
 if TYPE_CHECKING:
     from kandy_kafka.bootstrap import Bootstraped
 
-from textual.widgets import Footer, Header
+from textual.widgets import Footer
 from textual.app import App, ComposeResult
 from kandy_kafka.gui.views import TopicsView
 
@@ -13,24 +13,18 @@ from kandy_kafka.gui.views import TopicsView
 class Controller(App):
     """Main application controller"""
 
-    BINDINGS = [("ctrl+r", "reload", "reload")]
+    BINDINGS = [
+        ("ctrl+l", "focus_next"),
+    ]
     CSS_PATH = "app.css"  # Textual CSS file for styling
 
     def __init__(self, bootstraped: Bootstraped):
         super().__init__()
-        self.bootstrapped = bootstraped
-        self.kafka_adapter = self.bootstrapped.kafka_adapter
-        self.view = TopicsView()  # Instantiate the TopicsView from views.py
-
-    async def on_mount(self):
-        await self.reload_topics()
+        self.view = TopicsView(bootstraped)  # Instantiate the TopicsView from views.py
+        self.bind("ctrl+j", "focus_previous")
 
     async def reload_topics(self):
-        topics = self.kafka_adapter.get_topics()  # Fetch topics from Kafka
-        self.view.show_topics(topics)  # Update the table in the view
-
-    async def action_reload(self) -> None:
-        await self.reload_topics()
+        self.view.load_topics()  # Update the table in the view
 
     def compose(self) -> ComposeResult:
         yield self.view
--- kandy_kafka/gui/views/topics.py
+++ kandy_kafka/gui/views/topics.py
@@ -1,3 +1,9 @@
+from __future__ import annotations
+from typing import TYPE_CHECKING, Tuple
+
+if TYPE_CHECKING:
+    from kandy_kafka.bootstrap import Bootstraped
+
 from textual.containers import Horizontal, Vertical, Container
 from textual.reactive import reactive
 from textual.app import ComposeResult
@@ -7,69 +13,95 @@ from textual.widgets import (
     ListView,
     Input,
     DataTable,
+    LoadingIndicator,
 )
 from kandy_kafka.domain.models import Topic
 
 import re
+from typing import List
 
 
-class TopicsDataTable(DataTable):
-    BINDINGS = [("m", "sort('messages')", "Sort by messages")]
+class BaseTable(DataTable):
+    BINDINGS = [("j", "cursor_down"), ("k", "cursor_up")]  # Vim bindings
 
     def __init__(self, id: str):
         super().__init__(id=id)
-
         # Dictionary to store sorting directions for each column
-        self.sort_directions = {
-            "Number of messages": False,  # False = Ascending, True = Descending
-            "Partitions": False,
-        }
+        self.sort_directions = {}
 
-    def build_table(self):
+    def build_table(self, columns: List[Tuple[str, str]]):
         """Build the DataTable for topics."""
-        self.clear()
         self.cursor_type = "row"
         self.zebra_stripes = True
 
-        for col in (
-            "Topic name",
-            "Partitions",
-            "Replication Factor",
-            "Number of messages",
-        ):
-            self.add_column(col, key=col)
+        for column_name, key in columns:
+            self.add_column(column_name, key=key)
 
-    def update_table(self, topics: list[Topic]):
+    def update_rows(self, rows: List[List]):
         """Update the table with the provided list of topics."""
         self.clear()
 
+        for row in rows:
+            self.add_row(*row)
+
+    # def toggle_sort_direction(self, column: str) -> bool:
+    #     """Toggle the sort direction for the given column."""
+    #     # Flip the sort direction and store it
+    #     self.sort_directions[column] = not self.sort_directions[column]
+    #     return self.sort_directions[column]
+    #
+    # def get_sort_key(self, column: str):
+    #     """Return the appropriate key function for sorting based on the column."""
+    #     return lambda topic: topic.name  # Default to sorting by topic name
+    #
+    # def sort_table(self, topics: list[Topic], column: str) -> list[Topic]:
+    #     """Sort the topics list by the specified column."""
+    #     reverse = self.toggle_sort_direction(column)
+    #     key_func = self.get_sort_key(column)
+    #     return sorted(topics, key=key_func, reverse=reverse)
+
+
+class MessagesTable(BaseTable):
+    def __init__(self) -> None:
+        super().__init__("messages-table")
+
+    def on_mount(self):
+        self.build_table(
+            [
+                ("Offset", "offset"),
+                ("Partition", "partition"),
+                ("Timestamp", "timestamp"),
+                ("Key", "key"),
+                ("Value", "Value"),
+            ]
+        )
+        self.border_title = "Messages"
+
+
+class TopicsTable(BaseTable):
+    def __init__(self) -> None:
+        super().__init__("topics-table")
+
+    def on_mount(self):
+        self.build_table(
+            [
+                ("Topic name", "name"),
+                ("Partitions", " partitions"),
+                ("Replication factor", "replication"),
+                ("Amount of Messages", "messages"),
+            ]
+        )
+        self.border_title = "Topics"
+
+    def update_topics_in_table(self, topics: List[Topic]):
+        """Update the table with the provided list of topics."""
+        rows = []
         for topic in topics:
-            self.add_row(
-                topic.name,
-                len(topic.partitions),
-                1,  # Hardcoded Replication Factor, until I find a way to fetch this info from kafka
-                topic.amount_of_messages,
+            rows.append(
+                [topic.name, len(topic.partitions), 1, topic.amount_of_messages]
             )
 
-    def toggle_sort_direction(self, column: str) -> bool:
-        """Toggle the sort direction for the given column."""
-        # Flip the sort direction and store it
-        self.sort_directions[column] = not self.sort_directions[column]
-        return self.sort_directions[column]
-
-    def get_sort_key(self, column: str):
-        """Return the appropriate key function for sorting based on the column."""
-        if column == "Number of messages":
-            return lambda topic: topic.amount_of_messages
-        elif column == "Partitions":
-            return lambda topic: len(topic.partitions)
-        return lambda topic: topic.name  # Default to sorting by topic name
-
-    def sort_table(self, topics: list[Topic], column: str) -> list[Topic]:
-        """Sort the topics list by the specified column."""
-        reverse = self.toggle_sort_direction(column)
-        key_func = self.get_sort_key(column)
-        return sorted(topics, key=key_func, reverse=reverse)
+        self.update_rows(rows)
 
 
 class SearchHandler:
@@ -87,17 +119,18 @@ class SearchHandler:
 
 
 class TopicsView(Container):
-    """Main view to display topics in a table with search and sorting."""
+    BINDINGS = [
+        ("ctrl+r", "reload", "reload"),
+    ]
 
-    def __init__(self):
+    def __init__(self, bootstraped: Bootstraped):
         super().__init__()
-        self.topics_table = TopicsDataTable(id="topics-table")
-        self.messages_table = DataTable(id="messages-table")
-        self.topics = []  # Store all topics
-        self.filtered_topics = reactive([])  # Reactive field for filtered topics
+        self.kafka_adapter = bootstraped.kafka_adapter
+
+        self.topics_table = TopicsTable()
+        self.messages_table = MessagesTable()
 
     def compose(self) -> ComposeResult:
-        """Compose the view with the table, search, and navigation."""
         yield Horizontal(
             Vertical(
                 ListView(
@@ -108,7 +141,9 @@ class TopicsView(Container):
                 id="sidebar",
             ),
             Vertical(
-                Input(placeholder="Search Topics...", id="search-field"),
+                Input(
+                    placeholder="Search Topics...", id="search-field"
+                ),  # Create class that will work with input
                 Horizontal(
                     self.topics_table,
                     self.messages_table,
@@ -117,34 +152,22 @@ class TopicsView(Container):
             ),
         )
 
-    async def on_mount(self):
-        """Called when app is mounted, build the initial table."""
-        self.topics_table.build_table()
-        self.topics_table.border_title = "Topics"
-        self.topics_table.focus()
+    def on_mount(self):
+        self.load_topics()
 
-    def show_topics(self, topics: list[Topic]):
-        """Show the given list of topics in the table."""
+    def load_topics(self):
+        self.topics_table.loading = True
+        topics = self.kafka_adapter.get_topics()
         self.topics = topics  # Store the topics for later filtering
-        self.filtered_topics = topics  # Initially, filtered topics = all topics
-        self.topics_table.update_table(self.filtered_topics)
-
-    def action_sort_by_messages(self) -> None:
-        """Sort the table by the size column."""
-        self.filtered_topics = self.topics_table.sort_table(
-            self.filtered_topics, "Size"  # pyright: ignore[reportArgumentType]
-        )
-        self.topics_table.update_table(self.filtered_topics)
-
-    def action_sort_by_partitions(self) -> None:
-        """Sort the table by the number of partitions."""
-        self.filtered_topics = self.topics_table.sort_table(
-            self.filtered_topics, "Partitions"  # pyright: ignore[reportArgumentType]
-        )
-        self.topics_table.update_table(self.filtered_topics)
+        self.topics_table.update_topics_in_table(self.topics)
+        self.topics_table.loading = False
 
     async def on_input_changed(self, event):
         """Called whenever the input in the search field changes."""
         search_query = event.value.strip()
-        self.filtered_topics = SearchHandler.filter_topics(self.topics, search_query)
-        self.topics_table.update_table(self.filtered_topics)
+        filtered_topics = SearchHandler.filter_topics(self.topics, search_query)
+        self.topics_table.update_topics_in_table(filtered_topics)
+
+    # Actions
+    def action_reload(self):
+        self.load_topics()
